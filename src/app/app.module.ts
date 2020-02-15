2d1f10e8b7d1e831746284d28ec38d45ada901b5
juan camilo <juancamilo.piedrahita@utp.edu.co>
2020-02-15 12:41:18 -0500

    entorno back y bootswatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './imagen/lista.component';
+import { NuevaComponent } from './imagen/nueva.component';
+import { DetalleComponent } from './imagen/detalle.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    NuevaComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModalModule
   ],
+  entryComponents: [DetalleComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
