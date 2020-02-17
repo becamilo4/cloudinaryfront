121e18be7fabd3a08e69632a5f1261ca6a367d4f
juan camilo <juancamilo.piedrahita@utp.edu.co>
2020-02-16 23:38:59 -0500

    componente de lista

--- src/app/imagen/lista.component.ts
+++ src/app/imagen/lista.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Imagen } from '../models/imagen';
+import { ImagenService } from '../services/imagen.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DetalleComponent } from './detalle.component';
 
 @Component({
   selector: 'app-lista',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  imagenes: Imagen[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private imagenService: ImagenService,
+    private spinner: NgxSpinnerService,
+    private modalService: NgbModal
+    ) { }
+
+  ngOnInit() {
+   this.cargarImagenes();
+  }
+
+  cargarImagenes(): void {
+    this.imagenService.list().subscribe(
+      data => {
+        this.imagenes = data;
+      }
+    );
+  }
+
+  borrar(id: number): void {
+    this.spinner.show();
+    this.imagenService.delete(id).subscribe(
+      data => {
+        this.spinner.hide();
+        this.cargarImagenes();
+      },
+      err => {
+        this.spinner.hide();
+        console.log(err);
+      }
+    );
+  }
+
+  abrirModal(i: number): void {
+    const modalRef = this.modalService.open(DetalleComponent);
+    modalRef.componentInstance.index = i;
   }
 
 }
